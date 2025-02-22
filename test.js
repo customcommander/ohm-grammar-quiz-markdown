79785661f07e9cbac9f12c3e8523b1cafa971b2e
customcommander <hello@spinjs.com>
2025-02-22 11:35:27 +0000

    move schemas to separate files

--- test.js
+++ test.js
@@ -1,72 +1,24 @@
+import {readFile} from 'node:fs/promises';
 import test from 'node:test';
 import Ajv from 'ajv';
 
 import {grammar,semantics} from './index.js';
-
-const validate = (
-  (new Ajv)
-  .addSchema({
-    '$id': 'customcommander/ohm-grammar-quiz-markdown/schemas/defs.json',
-    definitions: {
-      question: {
-        type: 'object',
-        properties: {
-          '@type': {
-            const: 'Question'
-          },
-          text: {
-            type: 'string'
-          },
-          acceptedAnswer: {
-            '$ref': 'defs.json#/definitions/answer'
-          }
-        },
-        required: ['@type', 'acceptedAnswer', 'text']
-      },
-      answer: {
-        type: 'object',
-        properties: {
-          '@type': {
-            const: 'Answer'
-          },
-          position: {
-            type: 'number'
-          },
-          text: {
-            type: 'string'
-          }
-        },
-        required: ['@type', 'position', 'text']
-      }
-    }
-  })
-  .compile({
-    '$id': 'customcommander/ohm-grammar-quiz-markdown/schemas/main.json',
-    type: 'object',
-    properties: {
-      '@context': {
-        const: 'https://schema.org/'
-      },
-      '@type': {
-        const: 'Quiz'
-      },
-      hasPart: {
-        type: 'array',
-        items: {
-          '$ref': 'defs.json#/definitions/question'
-        }
-      },
-    },
-    required: ['@context', '@type', 'hasPart']
-  })
-);
 
 function parse(md_string) {
   const match = grammar.match(md_string);
   return semantics(match).schema();
 }
 
-test('parse markdown', () => {
+test('parse markdown', async () => {
+  const main = await readFile('./schema.main.json'       , {encoding: 'utf8'});
+  const defs = await readFile('./schema.definitions.json', {encoding: 'utf8'});
+  
+  const validate = (
+    (new Ajv)
+      .addSchema(JSON.parse(defs))
+      .compile(JSON.parse(main))
+  );
+
   const quiz = parse(`
 
     What is the correct answer?
