23525b085d027c44b6b495a2b9732011b20113d9
customcommander <hello@spinjs.com>
2025-02-22 11:11:34 +0000

    json-schema test

--- test.js
+++ test.js
@@ -1,5 +1,65 @@
 import test from 'node:test';
+import Ajv from 'ajv';
+
 import {grammar,semantics} from './index.js';
+
+const validate = (
+  (new Ajv)
+  .addSchema({
+    '$id': 'customcommander/ohm-grammar-quiz-markdown/schemas/defs.json',
+    definitions: {
+      question: {
+        type: 'object',
+        properties: {
+          '@type': {
+            const: 'Question'
+          },
+          text: {
+            type: 'string'
+          },
+          acceptedAnswer: {
+            '$ref': 'defs.json#/definitions/answer'
+          }
+        },
+        required: ['@type', 'acceptedAnswer', 'text']
+      },
+      answer: {
+        type: 'object',
+        properties: {
+          '@type': {
+            const: 'Answer'
+          },
+          position: {
+            type: 'number'
+          },
+          text: {
+            type: 'string'
+          }
+        },
+        required: ['@type', 'position', 'text']
+      }
+    }
+  })
+  .compile({
+    '$id': 'customcommander/ohm-grammar-quiz-markdown/schemas/main.json',
+    type: 'object',
+    properties: {
+      '@context': {
+        const: 'https://schema.org/'
+      },
+      '@type': {
+        const: 'Quiz'
+      },
+      hasPart: {
+        type: 'array',
+        items: {
+          '$ref': 'defs.json#/definitions/question'
+        }
+      },
+    },
+    required: ['@context', '@type', 'hasPart']
+  })
+);
 
 function parse(md_string) {
   const match = grammar.match(md_string);
@@ -8,23 +68,20 @@ function parse(md_string) {
 
 test('parse markdown', () => {
   const quiz = parse(`
-Question A
 
-- [ ] Answer 1
-- [ ] Answer 2
-- [x] Answer 3
+    What is the correct answer?
 
-Question B
+    - [ ] 41
+    - [x] 42
+    - [ ] 43
 
-- [ ] Answer 4
-- [x] Answer 5
+  `);
 
-Question C
+  if (validate(quiz) !== true) {
+    throw new Error('fail');
+  }
 
-- [x] Answer 6
-- [ ] Answer 7
-- [x] Answer 8
-`);
+  console.log(JSON.stringify(quiz, null, 2));
 
 });
 
